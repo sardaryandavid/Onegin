dc8db3781f251207810fce9fefb1d793c5d5ce85
sardaryandavid <74458820+sardaryandavid@users.noreply.github.com>
2021-09-14 09:06:58 +0300

    Add files via upload

--- SortFile/main.cpp
+++ SortFile/main.cpp
@@ -8,14 +8,13 @@
 
 #define TESTS
 
-void printText(char** arrayOfptrOnStrings, const int linesNumber);
-char* readText(FILE* text);
+void printText(char** arrayOfptrOnStrings, const size_t linesNumber);
+char* readText(FILE* text, char* str, const size_t fileSize);
 
 int sizeOfFile(FILE* text);
-int linesNumber(char* str, const size_t fileSize);
+int linesNumber(const char* const str, const size_t fileSize);
 
 void fillArray(char** arrayOfptrOnStrings, char* str, const size_t fileSize);
-void sortArray(char** arrayOfptrOnStrings, const int nLines);
 
 int myStrcmp(const void* str1, const void* str2);
 
@@ -36,25 +35,24 @@ void launchProgram() {
     FILE* text = fopen("text.txt" , "r");
 
     size_t fileSize = sizeOfFile(text);
+    char* str = (char*) calloc(fileSize, sizeof(*str));
+    readText(text, str, fileSize);
 
-    char* str = readText(text);
-
-    int nLines = linesNumber(str, fileSize);
-
-    char** arrayOfptrOnStrings = (char**) calloc (nLines, sizeof(char *));
-
+    size_t nLines = linesNumber(str, fileSize);
+    char** arrayOfptrOnStrings = (char**) calloc (nLines, sizeof(*arrayOfptrOnStrings));
     fillArray(arrayOfptrOnStrings, str, fileSize);
 
-    printText(arrayOfptrOnStrings, nLines);
-
-    //sortArray(arrayOfptrOnStrings, nLines);
-    qsort(arrayOfptrOnStrings, fileSize, sizeof(char*), myStrcmp);
-
+    printText(arrayOfptrOnStrings, nLines); //!
+    qsort(arrayOfptrOnStrings, nLines, sizeof(*arrayOfptrOnStrings), myStrcmp);
     printf("\n\n\n");
     printText(arrayOfptrOnStrings, nLines);
+
+    free(arrayOfptrOnStrings);
+    free(str);
+    fclose(text);
 }
 
-void printText(char** arrayOfptrOnStrings, int linesNumber) {
+void printText(char** arrayOfptrOnStrings, const size_t linesNumber) {
     for(int testIndex = 0; testIndex < linesNumber; ++testIndex) {
         if (arrayOfptrOnStrings[testIndex] != nullptr) {
             printf("%s\n", arrayOfptrOnStrings[testIndex]);
@@ -62,23 +60,18 @@ void printText(char** arrayOfptrOnStrings, int linesNumber) {
     }
 }
 
-char* readText(FILE* text) {
-
+char* readText(FILE* text, char* str, const size_t fileSize) {
      if(text == NULL) {
         printf("Error, can`t find the file");
         exit(0);
     }
-
-    size_t fileSize = sizeOfFile(text);
-
-    char* str = (char*) calloc(fileSize, sizeof(char));
 
     if (str == NULL) {
         printf("Error. This text is too big");
         exit(1);
     }
 
-    fread(str, sizeof(char), fileSize, text);
+    fread(str, sizeof(*str), fileSize, text);
 
     return str;
 }
@@ -91,8 +84,9 @@ int sizeOfFile(FILE* text) {
     return fileSize;
 }
 
-int linesNumber(char* str, size_t fileSize) {
+int linesNumber(const char* const str, const size_t fileSize) {
     int linesNumber = 0;
+
     for(int i = 0; i < fileSize; ++i) {
         if(str[i] == '\n') {
             ++linesNumber;
@@ -102,37 +96,24 @@ int linesNumber(char* str, size_t fileSize) {
     return linesNumber;
 }
 
-void fillArray(char** arrayOfptrOnStrings, char* str, size_t fileSize) {
+
+void fillArray(char** arrayOfptrOnStrings, char* str, const size_t fileSize) {
     arrayOfptrOnStrings[0] = str;
 
     int currentLine = 1;
 
     for(int j = 0; j < fileSize; ++j) {
         if(str[j] == '\n') {
-            ++currentLine;
             str[j] = '\0';
             if(str + j + 1 != nullptr) {
                 arrayOfptrOnStrings[currentLine] = str + j + 1;
             }
+            ++currentLine;
         }
     }
 
 }
 
-void sortArray(char** arrayOfptrOnStrings, int nLines) {
-    for(int firstIndex = nLines - 1; firstIndex > 0; --firstIndex)
-        for(int secondIndex = 0; secondIndex < firstIndex; ++secondIndex) {
-            if(arrayOfptrOnStrings[secondIndex] != nullptr && arrayOfptrOnStrings[firstIndex] != nullptr  && arrayOfptrOnStrings[secondIndex+1] != nullptr) {
-                if (strcmp(arrayOfptrOnStrings[secondIndex], arrayOfptrOnStrings[secondIndex + 1]) > 0) {
-                    char* tmp = arrayOfptrOnStrings[secondIndex];
-                    arrayOfptrOnStrings[secondIndex] = arrayOfptrOnStrings[secondIndex + 1];
-                    arrayOfptrOnStrings[secondIndex + 1] = tmp;
-                }
-            }
-        }
-}
-
-int myStrcmp(const void* str1, const void* str2) {
-    strcmp((char*)str1, (char*)str2);
-    return 1;
+int myStrcmp(const void* const str1, const void* const str2) {
+    return strcmp(*(char**) str1, *(char**) str2);
 }
